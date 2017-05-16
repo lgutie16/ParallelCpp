d02ee686fa736d40bb289d26aff3a774bc560b8b
Linda J Gutiérrez <lindaandgod@gmail.com>
2017-05-15 22:59:57 -0500

    No cildren calls - error in tubes

--- ctrl.cpp
+++ ctrl.cpp
@@ -44,18 +44,19 @@ WriteIn createTube(string& childInfo, int& position) {
 
   //Start tube creation
   WriteIn wiFil;  //si esto cambia todo cambia
+  pipe(wiFil.pipeIn);
+  pipe(wiFil.pipeOut);
 
   pid_t child;
   wiFil.child = child;
-
-  pipe(wiFil.pipeIn);
-  pipe(wiFil.pipeOut);
 
   if ((wiFil.child = fork()) == 0) {
     close(wiFil.pipeOut[1]);
     close(wiFil.pipeIn[0]);
     dup2(wiFil.pipeOut[0], 0);
     dup2(wiFil.pipeIn[1], 1);
+    close(wiFil.pipeOut[0]);    
+    close(wiFil.pipeIn[1]);
     execl("./mayusculas", "mayusculas", NULL);
     return wiFil;
   }
@@ -73,15 +74,17 @@ main(void) {
      //Creo las tuberias para cada proceso con una funcion -  createTube()
         //Vector para almacenar cada proceso con sus tuberias
   vector<WriteIn> TubesReference; 
+
   const char* file = "ctrsis.cfg";
   int levelOneProcesses = 0;
   string line;
   ifstream configFile(file);
+
   if(configFile.is_open()){
     while ( getline (configFile,line) ){
       WriteIn structure = createTube(line, levelOneProcesses);
-      TubesReference.push_back(structure);     
-      levelOneProcesses++;
+      TubesReference.push_back(structure);
+      levelOneProcesses++;    
     }
   }else{
     cout << "Unable to open file" << endl; 
@@ -90,18 +93,21 @@ main(void) {
   //Ya tengo los procesos guardados en un vector
   //Ahora asocio las entradas con las salidas de cada una de las tuberias
   //Según corresponda
+
   WriteIn mainTube;
-  if ((mainTube.in = open("mayusculas.cpp", O_RDONLY)) == -1) {
+  TubesReference.push_back(mainTube);
+  if ((TubesReference[0].in = open("mayusculas.cpp", O_RDONLY)) == -1) {
     std::cerr << "Error open file" << std::endl;
     return 1;
   }
-  for (int i = 0; i < TubesReference.size(); ++i){
+
+  TubesReference[0].out = TubesReference[1].pipeOut[1];
+  for (int i = 1; i < TubesReference.size()-1; ++i){
     TubesReference[i].in = TubesReference[i].pipeIn[0];
     TubesReference[i].out = TubesReference[i+1].pipeOut[1];
+    TubesReference[i+1].in = TubesReference[i+1].pipeIn[0];
   }
-  //TubesReference[TubesReference.size()-1].out = 1;
-
-  
+  TubesReference[TubesReference.size()-1].out = 1;
 
   //Con ese array creo los hilos que van a ejecutar a los ctrEval 
   //Aqué quedaría listo el primer nivel
@@ -112,11 +118,11 @@ main(void) {
     levelOneThreads[i] = Proccessthread;
   }
 
-  pthread_create(&levelOneThreads[0], NULL, readWriteThread, &mainTube);
-  for (int i=1; i < TubesReference.size()+1; ++i){
-    cout << "creating threads" <<  endl;
+  pthread_create(&levelOneThreads[0], NULL, readWriteThread, &TubesReference[0]);
+  pthread_create(&levelOneThreads[1], NULL, readWriteThread, &TubesReference[1]);
+  /*for (int i=1; i < TubesReference.size()+1; ++i){
     pthread_create(&levelOneThreads[i], NULL, readWriteThread, &TubesReference[i-1]);
-  }
+  }*/
 
   void *ret;
   pthread_join(levelOneThreads[0], &ret);
